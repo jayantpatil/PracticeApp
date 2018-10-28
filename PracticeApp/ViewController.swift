2a0b2026b7a063a46fe8f424dd1c94b8cdbe926f
Jayant Patil <jayant.ptl@gmail.com>
2018-10-28 21:13:13 +0530

    Initial commit

---
+++ PracticeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeApp
+//
+//  Created by Jayant on 28/10/18.
+//  Copyright © 2018 Jayant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
